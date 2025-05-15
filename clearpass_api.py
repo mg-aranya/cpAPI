c905a3dabcda6684eace349885f8d44acfdb107a
mg-aranya <mathias.granlund@aranya.se>
2025-05-15 13:24:32 +0200

    Update clearpass_api.py

--- clearpass_api.py
+++ clearpass_api.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-# ====================================================================================
+# ======================================================================
 # title             :clearpass_api.py
-# description       :Script using ClearPass API to read and modify configuration
+# description       :uses ClearPass API to read and modify configuration
 # author            :Mathias Granlund [mathias.granlund@aranya.se]
 # date              :20250501
 # version           :0.1
@@ -13,57 +13,31 @@
 # notes             :this is a work in progress, features will be added over time
 #                   :ues at your own risk
 # python_version    :3.10.12
-# ====================================================================================
-from pyclearpass import *
-from ARApy import *
+# ======================================================================
 import json
 import requests
-import argparse
 import csv
 import os
 import sys
 import time
-
+import argparse
+from pyclearpass import *
+from ARApy import *
 
 def main():
     args = arguments()
-    while True:
-        if args.menu:
-            try:
-                menu()
-                choice = input("Enter your choice: ").strip().lower()
-                match choice:
-                    case '1':
-                        json_data = Cget_network_device(credentials())
-                    case '2':
-                        json_data = Cget_network_device_name_by_name(credentials())
-                    case '3':
-                        json_data = Cnew_network_device(credentials())
-                    case '4':
-                        json_data = Cdelete_network_device_name_by_name(credentials())
-                    case '5':
-                        pass
-                    case '6':
-                        pass
-                    case '7':
-                        pass
-                    case '8':
-                        pass
-                    case '9':
-                        pass
-                    case '10':
-                        pass 
-                    case '99':
-                        sys.exit()
-                    case _:
-                        parse.print_help()
-                        print("Invalid choice.")
-            except (KeyboardInterrupt, EOFError) as error:
-                print("\nControl-C... Exiting")
-                sys.exit()
+    if args.menu:
+        menu()
+    elif args.device:
+        if args.list:
+            json_data = Cget_network_device(credentials())
+        if args.add:
+            json_data = Cnew_network_device(credentials(), args.description, args.name, args.ipv4, args.RADIUSKEY, args.TACACSKEY, args.vendor)
+    elif args.cert:
+        if args.list:
+            json_data = Cget_cert_trust_list(credentials())
         else:
-            print(args)
-            break
+            print("Invalid args")
 
 if __name__ == "__main__":
     main()
